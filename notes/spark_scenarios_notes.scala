b1ec7257444a175d4a2144da9855560165f55576
Rajesh Santha <rajesh.santha29@gmail.com>
2019-01-31 14:07:40 -0500

    saved input for scenario

---
+++ notes/spark_scenarios_notes.scala
@@ -0,0 +1,12 @@
+scala> val df1 = spark.read.format("csv").option("header","true").option("sep",";").load("/user/rajeshs/datasets/scenarios/lic.csv")
+df1: org.apache.spark.sql.DataFrame = [id: string, Name: string ... 6 more fields]
+
+scala> df1.show
++---+-----------+----------+--------+-----------+--------------+----------+----------+
+| id|       Name|   Address|    type|enrolled_on|          bank|       dob|     phone|
++---+-----------+----------+--------+-----------+--------------+----------+----------+
+|  1|Ram Chandra|Ayodhya UP| Primium| 01-10-2010|retail-banking|02-10-1980|9999999999|
+|  2|     Laxman|Ayodhya UP|Standard| 01-11-2011|    Creditcard|01-11-1966|9999999910|
++---+-----------+----------+--------+-----------+--------------+----------+----------+
+
+
